bed8b5827e99ab80c163e070e2313fa5b71d8316
Samark Chaisanguan <samarkchsngn@gmail.com>
2018-06-29 14:28:22 +0700

    add simple test

---
+++ tests/HelperTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Samark\Helper\Tests;
+
+class BlacklistTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHelper()
+    {
+        $this->assertTrue(true);
+    }
+}
